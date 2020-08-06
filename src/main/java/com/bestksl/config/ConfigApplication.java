7836d04372ed324c688a64fbf25170cf8460747e
will <will@skyland.net.cn>
2020-08-06 09:46:44 +0800

    Initial commit

---
+++ src/main/java/com/bestksl/config/ConfigApplication.java
@@ -0,0 +1,15 @@
+package com.bestksl.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bestksl/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestksl.config;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
